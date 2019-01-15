a33a60e4f5c9ad579c08676865ed3e2baa704396
Sergey <sergeyakkaynt@gmail.com>
2019-01-15 20:08:02 +0300

    Выносит лишнее в другой модуль

--- js/util.js
+++ js/util.js
@@ -1,60 +1,5 @@
 'use strict';
 (function () {
-  /*var COAT = 'rgb(101, 137, 164)';
-  var EYES = 'black';
-  var coatColor = COAT;
-  var eyesColor = EYES;
-  var wizards = [];
-
-  var getRank = function (wizard) {
-    var rank = 0;
-
-    if (wizard.colorCoat === coatColor) {
-      rank += 2;
-    }
-    if (wizard.colorEyes === eyesColor) {
-      rank += 1;
-    }
-
-    return rank;
-  };
-
-  var namesComparator = function (left, right) {
-    if (left > right) {
-      return 1;
-    } else if (left < right) {
-      return -1;
-    } else {
-      return 0;
-    }
-  };
-
-  var updateWizards = function () {
-    window.render.render(wizards.sort(function (left, right) {
-      var rankDiff = getRank(right) - getRank(left);
-      if (rankDiff === 0) {
-        rankDiff = namesComparator(left.name, right.name);
-      }
-      return rankDiff;
-    }));
-  };
-
-
-  var eyesChangeHandler = window.debounce.debounce(function (color) {
-    eyesColor = color;
-    updateWizards();
-  });
-
-  var coatChangeHandler = window.debounce.debounce(function (color) {
-    coatColor = color;
-    updateWizards();
-  });*/
-
-  var loadHandler = function (data) {
-    window.filter.wizards = data;
-    window.filter.updateWizards();
-  };
-
   var errorHandler = function (errorMessage) {
     var node = document.createElement('div');
     node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
@@ -74,14 +19,8 @@
 
   };
 
-  var request = function () {
-    window.backend.load(loadHandler, errorHandler);
-  };
-
   window.util = {
-    loadHandler: loadHandler,
-    errorHandler: errorHandler,
-    request: request,
+    errorHandler: errorHandler
   };
 })();
 
